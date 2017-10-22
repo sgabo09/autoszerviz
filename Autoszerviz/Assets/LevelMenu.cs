90e2088f1d5380af530d4e76a460cd3171eceda4
F3c0k4 <zagferenc@gmail.com>
2017-10-22 10:29:01 +0200

    Added menu
    Menu now works with
    New Game -> First Level
    and possibly the quit button

---
+++ Autoszerviz/Assets/LevelMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenu : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Level 1"))
+        {
+            Application.LoadLevel(0);
+        }
+        if (GUI.Button(new Rect(Screen.width / 2.5f,  Screen.height / 2, Screen.width / 5, Screen.height / 10), "Level 2"))
+        {
+            Application.LoadLevel(1);
+        }
+       
+
+    }
+}
---
+++ Autoszerviz/Assets/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/5,Screen.width/5,Screen.height/10),"New Game"))
+        {
+            Application.LoadLevel(3);
+        }
+        if (GUI.Button(new Rect(Screen.width / 2.5f, 2*Screen.height /5, Screen.width / 5, Screen.height / 10), "Settings"))
+        {
+            Application.LoadLevel(1);
+        }
+        if (GUI.Button(new Rect(Screen.width / 2.5f,3* Screen.height / 5, Screen.width / 5, Screen.height / 10), "High Score"))
+        {
+            Application.LoadLevel(1);
+        }
+        if (GUI.Button(new Rect(Screen.width / 2.5f, 4*Screen.height / 5, Screen.width / 5 , Screen.height / 10), "Quit"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
