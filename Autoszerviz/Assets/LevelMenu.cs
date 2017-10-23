3c8a0fd6eae6f225a3687f58220d8b54e240b727
sgabo09 <32134657+sgabo09@users.noreply.github.com>
2017-10-23 15:51:50 +0200

    bug fixes
    -Kameraváltásnál működik a hang
    -A menü scriptekben nem beégetett értékek vannak, editorból cserélhetőek

--- Autoszerviz/Assets/LevelMenu.cs
+++ Autoszerviz/Assets/LevelMenu.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMenu : MonoBehaviour
 {
+    public string level1, level2;
     private void OnGUI()
     {
         if (GUI.Button(new Rect(Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Level 1"))
         {
-            Application.LoadLevel(0);
+            SceneManager.LoadScene(level1,LoadSceneMode.Single);
         }
         if (GUI.Button(new Rect(Screen.width / 2.5f,  Screen.height / 2, Screen.width / 5, Screen.height / 10), "Level 2"))
         {
-            Application.LoadLevel(1);
+            SceneManager.LoadScene(level2,LoadSceneMode.Single);
         }
        
 
--- Autoszerviz/Assets/Menu.cs
+++ Autoszerviz/Assets/Menu.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
+    public string newGame, settings, highScore;
+
     private void OnGUI()
     {
         if(GUI.Button(new Rect(Screen.width/2.5f, Screen.height/5,Screen.width/5,Screen.height/10),"New Game"))
         {
-            Application.LoadLevel(3);
+            SceneManager.LoadScene(newGame, LoadSceneMode.Single);
         }
         if (GUI.Button(new Rect(Screen.width / 2.5f, 2*Screen.height /5, Screen.width / 5, Screen.height / 10), "Settings"))
         {
-            Application.LoadLevel(1);
+            SceneManager.LoadScene(settings, LoadSceneMode.Single);
         }
         if (GUI.Button(new Rect(Screen.width / 2.5f,3* Screen.height / 5, Screen.width / 5, Screen.height / 10), "High Score"))
         {
-            Application.LoadLevel(1);
+            SceneManager.LoadScene(highScore, LoadSceneMode.Single);
         }
         if (GUI.Button(new Rect(Screen.width / 2.5f, 4*Screen.height / 5, Screen.width / 5 , Screen.height / 10), "Quit"))
         {
