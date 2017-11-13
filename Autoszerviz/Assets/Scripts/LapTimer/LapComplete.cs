e658cf01848466840f95b6c809382e410c5d2e21
sgabo09 <32134657+sgabo09@users.noreply.github.com>
2017-11-14 00:21:39 +0100

    Timer beta

---
+++ Autoszerviz/Assets/Scripts/LapTimer/LapComplete.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour {
+
+    public GameObject LapCompleteTrigger;
+    public GameObject HalfLapTrigger;
+
+    public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter(){
+        if (LapTimeManager.secondCount <= 9)
+        {
+            SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.secondCount + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<Text>().text = "" + LapTimeManager.secondCount + ".";
+        }
+
+        if (LapTimeManager.minuteCount <= 9)
+        {
+            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.minuteCount + ".";
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.minuteCount + ".";
+        }
+
+        MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.milliCount;
+
+        LapTimeManager.minuteCount = 0;
+        LapTimeManager.secondCount = 0;
+        LapTimeManager.milliCount = 0;
+
+        HalfLapTrigger.SetActive(true);
+        LapCompleteTrigger.SetActive(false);
+    }
+}
---
+++ Autoszerviz/Assets/Scripts/OnTriggerLoadLevel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class OnTriggerLoadLevel : MonoBehaviour {
+    
+	public GameObject enterText;
+    public GameObject panel;
+    public Button exitButton;
+    public Button topSpeedPlusButton;
+    public Text ts;
+    public string level;
+    public GameObject car;
+    public CarController carController;
+    public float topSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		enterText.SetActive (false);
+        panel.SetActive(false);
+        topSpeedPlusButton.onClick.AddListener(TopSpeedPlus);
+        Button btn = exitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        carController = car.GetComponent<CarController>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+		if (other.gameObject.tag == "Player") 
+		{
+			enterText.SetActive (true);
+			if(enterText.activeInHierarchy == true && Input.GetButtonDown("Use"))
+			{
+                panel.SetActive (true);
+                enterText.SetActive (false);
+                Time.timeScale = 0;
+                topSpeed = carController.MaxSpeed;
+                ts.text = "Current top speed: " + topSpeed;
+			}
+		}
+	}
+
+	void OnTriggerExit () {
+		enterText.SetActive (false);
+	}
+
+    void TopSpeedPlus(){
+        carController.m_Topspeed = carController.m_Topspeed + 50;
+    }
+
+    void TaskOnClick(){
+        panel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
