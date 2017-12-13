90e42cdb4c3ef4c558ad85d8be11c5c3480b8532
sgabo09 <32134657+sgabo09@users.noreply.github.com>
2017-12-13 08:55:40 +0100

    beta

--- Autoszerviz/Assets/Scripts/LevelMenu.cs
+++ Autoszerviz/Assets/Scripts/LevelMenu.cs
@@ -8,11 +8,11 @@ public class LevelMenu : MonoBehaviour
     public string level1, level2;
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Level 1"))
+        if (GUI.Button(new Rect(Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Brands Hatch"))
         {
             SceneManager.LoadScene(level1,LoadSceneMode.Single);
         }
-        if (GUI.Button(new Rect(Screen.width / 2.5f,  Screen.height / 2, Screen.width / 5, Screen.height / 10), "Level 2"))
+        if (GUI.Button(new Rect(Screen.width / 2.5f,  Screen.height / 2, Screen.width / 5, Screen.height / 10), "Sentul Race Track"))
         {
             SceneManager.LoadScene(level2,LoadSceneMode.Single);
         }
